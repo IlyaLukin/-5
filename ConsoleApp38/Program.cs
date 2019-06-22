b46dc2e3dddb93823702f77ffc54729a5482b24c
rikoshet2018 <48294373+rikoshet2018@users.noreply.github.com>
2019-06-22 21:36:06 +0500

    Update Program.cs

--- ConsoleApp38/Program.cs
+++ ConsoleApp38/Program.cs
@@ -32,19 +32,36 @@ namespace Exercise5
                     Console.Write(mas[i, j].ToString() + " ");
                 Console.WriteLine();
             }
-
-
-            double maxElement = mas[0, 0];
-
-            foreach (double element in mas)
+            
+                double max = -1000;
+            double[] maxValues = new double[mas.GetLength(0)];
+            for (int i = 0; i < mas.GetLength(0); i++)
             {
-                if(maxElement<element)
+                for (int j = 0; j < mas.GetLength(1); j++)
                 {
-                    maxElement = element;
+                    if (mas[i, i] < 0)
+                    {
+                        if (mas[i, j] > max)
+                        {
+                            max = mas[i, j];
+                            maxValues[i] = max;
+                        }
+                    }
+                }
+                max = -1000;
+            }
+          
+            for (int i = 0; i < maxValues.Length; i++)
+            {
+                if (maxValues[i] == 0)
+                {
+                    Console.WriteLine($"В строке {i + 1}: главная диагональ не имеет отрицательного элемента");
+                }
+                else
+                {
+                    Console.WriteLine($"В строке {i + 1}: максимальный элемент {maxValues[i]}");
                 }
             }
-
-            Console.WriteLine($"Максимальный элемент матрицы {maxElement}");
             Console.ReadKey();
         }
     }
